b4daa84cca6db858b598f353f73a9a7ad748ed7d
JuanBautistaPenalvaPerez <juanbpp1@gmail.com>
2018-10-20 11:19:08 +0400

    creado las cadenas

--- src/basicosjuanbautista/BasicosJuanBautista.java
+++ src/basicosjuanbautista/BasicosJuanBautista.java
@@ -15,7 +15,35 @@ public class BasicosJuanBautista {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        cadenas();// TODO code application logic here
+    }
+
+     /**
+     * Método para mostrar ejemplos de tipos cadenas.
+     */
+    private static void cadenas() {
+    
+        //Estos son los tipos básicos
+        char cadena;
+        char cadena2;
+        
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        cadena = 'a';
+        cadena2 = 97;
+        String cadena3 = "Juan Bautista Penalva Perez";
+        
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor cadena = a");
+        System.out.println(cadena);
+        System.out.println("Valor cadena2 = 97");
+        System.out.println(cadena2);
+        System.out.println("Valor cadena3 = Juan Bautista Penalva Perez");
+        System.out.println(cadena3);
+        System.out.println("longitud cadena3 = Juan Bautista Penalva Perez");
+        System.out.println(cadena3.length());
+        System.out.println("caracter numero 10 de la cadena3 = Juan Bautista Penalva Perez");
+        System.out.println(cadena3.charAt(10));
+            
     }
     
 }
