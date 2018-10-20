bbf1d1993a8af00d019849308e0f8c97cd976848
JuanBautistaPenalvaPerez <juanbpp1@gmail.com>
2018-10-20 11:30:46 +0400

    creado los logicos

--- src/basicosjuanbautista/BasicosJuanBautista.java
+++ src/basicosjuanbautista/BasicosJuanBautista.java
@@ -15,7 +15,35 @@ public class BasicosJuanBautista {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        logicos();
         // TODO code application logic here
     }
+
+     /**
+     * Método para mostrar ejemplos de tipos logicos.
+     */
+    private static void logicos() {
+
+        //Estos son los tipos básicos
+        boolean verdadero;
+        boolean falso;
+
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        verdadero = true;
+        falso = false;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.println("Valor verdadero");
+        System.out.println(verdadero);
+        System.out.println("Valor falso");
+        System.out.println(falso);
+        System.out.println("Operadores logicos AND");
+        System.out.println(verdadero && falso);
+        System.out.println("Operadores logicos OR");
+        System.out.println(verdadero || falso);
+        System.out.println("Operadores logicos not sobre falso");
+        System.out.println(!falso);
+   }
     
 }
